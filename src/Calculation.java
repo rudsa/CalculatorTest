156f7134847b391c51862c3c5cf6a755236f12de
rudsa <rudsa@naver.com>
2021-12-06 23:37:26 +0900

    12-07

---
+++ src/Calculation.java
@@ -0,0 +1,49 @@
+public class Calculation {
+    private int num1;
+    private String cal;
+    private int num2;
+    private int result;
+
+    private int sum(){
+        result = num1 + num2;
+        return result;
+    }
+
+    private int minus(){
+        result = num1 - num2;
+        return result;
+    }
+
+    private void division(){
+
+    }
+
+    private void multifly(){
+
+    }
+
+    public String getCal() {
+        return cal;
+    }
+
+    public void setCal(String cal) {
+        this.cal = cal;
+    }
+
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+}
