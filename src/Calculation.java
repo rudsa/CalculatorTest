7f48e30700ce59aac7a2dfda398049bb0f6fda7f
rudsa <rudsa@naver.com>
2021-12-12 10:22:03 +0900

    test

--- src/Calculation.java
+++ src/Calculation.java
@@ -1,51 +1,75 @@
 public class Calculation {
-    private int num1;
-    private String cal;
-    private int num2;
-    private int result;
+    private double num1;
+    private String arithmeticOperation;
+    private double num2;
+    private double result;
 
-    private int sum(){
+    private double plus(){
         result = num1 + num2;
+        System.out.println(result);
         return result;
     }
 
-    private int minus(){
+    private double minus(){
         result = num1 - num2;
+        System.out.println(result);
         return result;
     }
 
     private double division(){
         result = num1 / num2;
+        System.out.println(result);
         return result;
     }
 
     private double multifly(){
         result = num1 * num2;
+        System.out.println(result);
         return result;
     }
 
-    public String getCal() {
-        return cal;
+    public String getArithmeticOperation() {
+        return arithmeticOperation;
     }
 
-    public void setCal(String cal) {
-        this.cal = cal;
+    public void setArithmeticOperation(String arithmeticOperation) {
+        this.arithmeticOperation = arithmeticOperation;
     }
 
-
-    public int getNum1() {
+    public double getNum1() {
         return num1;
     }
 
-    public void setNum1(int num1) {
+    public void setNum1(double num1) {
         this.num1 = num1;
     }
 
-    public int getNum2() {
+    public double getNum2() {
         return num2;
     }
 
-    public void setNum2(int num2) {
+    public void setNum2(double num2) {
         this.num2 = num2;
+    }
+
+    void calculationStart(){
+        switch (getArithmeticOperation()){
+            case "+" :{
+                plus();
+            }
+            case "-" :{
+                minus();
+            }
+            case "*" :{
+                multifly();
+            }
+            case "/" :{
+                division();
+            }
+            default:{
+                System.out.println(result);
+                break;
+            }
+        }
     }
 }
--- src/Calculator.java
+++ src/Calculator.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        int num1 = scan.nextInt();
-        String test = scan.next();
-        int num2 = scan.nextInt();
+        Calculation cal = new Calculation();
+        double num1 = scan.nextDouble();
+        cal.setNum1(num1);
+        String arithmeticOperation = scan.next();
+        cal.setArithmeticOperation(arithmeticOperation);
+        double num2 = scan.nextDouble();
+        cal.setNum2(num2);
+        cal.calculationStart();
     }
 }
